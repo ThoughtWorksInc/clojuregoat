722ee1a935c1f9cfdf23f0a4143d6667575a6f57
James Thompson <james.thompson@thoughtworks.com>
2015-04-15 23:51:54 +0100

    Fixed tests

--- test/goat/test/util/routes.clj
+++ test/goat/test/util/routes.clj
@@ -17,25 +17,25 @@
          (:body response)   => (contains "Not Found")))
 
   (fact "it shows the search page ok"
-    (let [response (-> (mock/request :get "/search")
+    (let [response (-> (mock/request :get "/a3")
                        app)]
           (:status response) => 200
           (:body response)   => (contains "This facility will search the ")))
 
   (fact "you can post blank to the search page and get the extra section at the bottom"
-    (let [response (-> (mock/request :post "/search")
+    (let [response (-> (mock/request :post "/a3")
                      app)]
       (:status response) => 200
       (:body response)   => (contains "returned no results")))
 
   (fact "you can post to the search page and get the query back in the response"
-    (let [response (-> (mock/request :post "/search",  {:query "verbatim safe query"})
+    (let [response (-> (mock/request :post "/a3",  {:query "verbatim safe query"})
                      app)]
       (:status response) => 200
       (:body response)   => (contains "verbatim safe query")))
 
   (fact "it will verbatim back terribly unsafe stuff"
-    (let [response (-> (mock/request :post "/search",  {:query "<script>alert('I am an alert box!');</script>"})
+    (let [response (-> (mock/request :post "/a3",  {:query "<script>alert('I am an alert box!');</script>"})
                      app)]
       (:status response) => 200
       (:body response)   => (contains "<script>alert('I am an alert box!');</script>"))))
